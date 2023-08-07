276b3d6a8d4432f4f5bccdc37e4bbe4f053ff560
darecoco <whaqlrh37@naver.com>
2023-08-07 14:35:29 +0900

    모듈 알아보기

---
+++ scripts/3-13(module).js
@@ -0,0 +1,17 @@
+// function log2(message){
+//     console.log(message);
+// }
+
+// function error2(message){
+//     console.error(message);
+// }
+
+export function log2(message){
+    console.log(message);
+}
+
+export function error2(message){
+    console.error(message);
+}
+
+export const PI = 3.14;
---
+++ scripts/3-13(ref).js
@@ -0,0 +1,2 @@
+import {log2} from "/scripts/3-13(module).js";
+log2("log2 출력");
